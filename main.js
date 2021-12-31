aa5c7d93bd44c704e99ffe126dfbbaaea12227df
Dcoder-ai <84121667+Dcoder-ai@users.noreply.github.com>
2021-12-31 11:41:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    background("white");
+
+}
+function clearCanvas() {
+    background("white")
+}
+function draw() {
+
+}
+function check_sketch() {
+
+}
+function updateCanvas() {
+    background("white")
+    Math.floor
+}
